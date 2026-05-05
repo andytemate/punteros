bf170c747a6d9edf95ead5564c29357c76dada1e
Laboratorio <laboratorio@udla-ec.int>
2026-05-05 11:43:22 -0500

    primer comit

---
+++ punteros.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main ( int argc, char const  *argv [])
+{
+
+
+int num= 15, a ;
+int *x;
+
+x= &num;  //a x se asigna la direccion de num
+a= *x; //
+printf(" %p %d\n", x,a);
+
+char letra = 'a',c;
+char * ptr;
+ptr= &letra;
+c= *ptr;
+printf("%p %c",ptr,c);
+
+return 0; 
+
+}
